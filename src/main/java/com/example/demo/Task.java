34af2d6fdc7cb967165752b1851e33f1190af55a
T X <tx@gmail.com>
2025-03-22 05:22:19 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Task.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+// import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean complated;
+
+}
