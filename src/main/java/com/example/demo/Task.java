577d4cbd56e77b436ade29da2395a89aa1c49c00
T X <tx@gmail.com>
2025-03-22 09:22:26 +0300

    izmeneniya)

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -15,4 +15,27 @@ public class Task {
     private String title;
     private boolean complated;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isComplated() {
+        return complated;
+    }
+
+    public void setComplated(boolean complated) {
+        this.complated = complated;
+    }
 }
